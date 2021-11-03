a88ced57a42eb6b534e8ab2efb8505f080965f02
thiagorafaelferreira <thiagorafaelferreira@gmail.com>
2021-11-03 13:52:44 -0300

    tic tac toe game

--- src/App.js
+++ src/App.js
@@ -1,90 +1,237 @@
+import React, { useState } from "react";
 
-import React, { useEffect, useState } from 'react';
-import  { BrowserRouter, NavLink, Route, Switch } from 'react-router-dom';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faSignOutAlt } from '@fortawesome/free-solid-svg-icons'
-import axios from 'axios';
-import './css/App.css';
-import './css/Global.css';
-import Home from './Home';
-import Login from './Login';
-import Register from './Register';
-import Plans from './Plans';
-import User from './User';
-import PublicRoute from './route/PublicRoute';
-import PrivateRoute from './route/PrivateRoute';
-import { getUser, getToken, setUserSession, resetSession } from './service/AuthService';
-import Logo from './img/logo.jpg';
+const rowStyle = {
+  display: "flex",
+};
 
+const squareStyle = {
+  width: "60px",
+  height: "60px",
+  backgroundColor: "#ddd",
+  margin: "4px",
+  display: "flex",
+  justifyContent: "center",
+  alignItems: "center",
+  fontSize: "40px",
+  color: "white",
+};
 
-import { getRequestHeader } from './service/RequestService'
+const boardStyle = {
+  backgroundColor: "#eee",
+  width: "208px",
+  alignItems: "center",
+  justifyContent: "center",
+  display: "flex",
+  flexDirection: "column",
+  border: "3px #eee solid",
+};
 
-const verifyUrl = "https://l7cevyfji1.execute-api.sa-east-1.amazonaws.com/dev/verify";
+const containerStyle = {
+  display: "flex",
+  alignItems: "center",
+  flexDirection: "column",
+};
 
-function App(props) {
+const instructionsStyle = {
+  marginTop: "5px",
+  marginBottom: "5px",
+  fontWeight: "bold",
+  fontSize: "16px",
+};
 
-  const [isAuthenticating, setAuthenticating] = useState(true);
-  const [user] = useState(getUser())
+const buttonStyle = {
+  marginTop: "15px",
+  marginBottom: "16px",
+  width: "80px",
+  height: "40px",
+  backgroundColor: "#8acaca",
+  color: "white",
+  fontSize: "16px",
+};
 
-  useEffect(() => {
-    const token = getToken();
-    if(token === 'undefined' || token === undefined || token === null || !token) {
-      return;
-    }
+const crossMark = {
+  backgroundColor: "blue",
+};
 
-    const user = getUser();
+const circleMark = {
+  backgroundColor: "green",
+};
 
-    const requestBody = {
-      token: token,
-      username: user
-    }
+const initialValues = {
+  winner: "None",
+  lastPlayed: "cross",
+  nextPlayer: "X",
+  positions: [0, 0, 0, 0, 0, 0, 0, 0, 0],
+};
 
-    axios.post(verifyUrl, requestBody, getRequestHeader()).then(response => {
-      setUserSession(response['username'], response['token']);
-      setAuthenticating(false);
-    }).catch(() => {
-      resetSession();
-      setAuthenticating(false);
-    })
-
-  }, []);
-
-  const token = getToken();
-
-  if (isAuthenticating && token) {
-    return <div className="content">Authenticating...</div>;
+class Square extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleOnClick = this.handleOnClick.bind(this);
   }
 
-  return (
-    <div className="App">
-      <BrowserRouter>
-        <div className="header">
-          <div className="logo">
-            <img className="logo-img" alt="logo" src={Logo} />
-          </div>
-          <div className="menu">
-            <NavLink exact activeClassName="active" to="/">Home</NavLink>
-            <NavLink activeClassName="active" to="/plans">Planos</NavLink>
-            <NavLink activeClassName="active" to="/register">Cadastro</NavLink>
-            <NavLink activeClassName="active" to="/login">Login</NavLink>
-            <NavLink activeClassName="active" to="/user">{ user !== undefined ? user : 'Usuário'}</NavLink>
-            {!isAuthenticating && token && <NavLink activeClassName="active" to="/signout"><FontAwesomeIcon icon={faSignOutAlt} /></NavLink>}
-          </div>
-        </div>
-        <div>
-          <Switch>
-            <Route exact path="/" component={Home} />
-            <PublicRoute path="/plans" component={Plans} />
-            <PublicRoute path="/register" component={Register} />
-            <PublicRoute path="/login" component={Login} />
-            <PrivateRoute path="/user" component={User} />
-            
-          </Switch>
-        </div>
-      </BrowserRouter>
-    </div>
-
-  );
+  handleOnClick() {
+    this.props.onClick(this.props.position);
+  }
+  render() {
+    let style = {};
+    let valuePosition = this.props.positions[this.props.position];
+    if (valuePosition !== 0)
+      style = valuePosition === 1 ? circleMark : crossMark;
+    return (
+      <div
+        onClick={() => this.props.onClick(this.props.position)}
+        className="square"
+        style={{ ...squareStyle, ...style }}
+      >
+        {valuePosition === 0 ? "" : valuePosition === 1 ? "O" : "X"}
+      </div>
+    );
+  }
 }
 
-export default App;
+class Board extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { ...initialValues };
+    this.handleClickSquare = this.handleClickSquare.bind(this);
+    this.handleClickReset = this.handleClickReset.bind(this);
+    this.isWinner = this.isWinner.bind(this);
+  }
+
+  handleClickReset() {
+    this.setState((prevState) => ({ ...initialValues }));
+  }
+
+  isWinner(positions) {
+    let winnerCombinations = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+    for (let i = 0; i < winnerCombinations.length; i++) {
+      let [a, b, c] = winnerCombinations[i];
+      if (
+        positions[a] !== 0 &&
+        positions[a] === positions[b] &&
+        positions[a] === positions[c]
+      )
+        return true;
+    }
+    return false;
+  }
+
+  handleClickSquare(position) {
+    console.log(position);
+    let alteredPositions = [...this.state.positions];
+    let lastPlayed = this.state.lastPlayed;
+    let nextPlayer = this.state.nextPlayer;
+    let winner = "None";
+
+    if (this.state.winner === "None" && alteredPositions[position] === 0) {
+      alteredPositions[position] = lastPlayed === "circle" ? 1 : 2;
+      lastPlayed = lastPlayed === "circle" ? "cross" : "circle";
+      nextPlayer = lastPlayed === "circle" ? "O" : "X";
+
+      if (this.isWinner(alteredPositions)) {
+        winner = this.state.nextPlayer;
+        alert(`${this.state.nextPlayer} wins!`);
+      }
+
+      this.setState((prevState) => ({
+        lastPlayed: lastPlayed,
+        positions: alteredPositions,
+        nextPlayer: nextPlayer,
+        winner: winner,
+      }));
+    }
+  }
+
+  render() {
+    return (
+      <div style={containerStyle} className="gameBoard">
+        <div id="statusArea" className="status" style={instructionsStyle}>
+          Next player: <span>{this.state.nextPlayer}</span>
+        </div>
+        <div id="winnerArea" className="winner" style={instructionsStyle}>
+          Winner: <span>{this.state.winner}</span>
+        </div>
+        <button style={buttonStyle} onClick={this.handleClickReset}>
+          Reset
+        </button>
+        <div style={boardStyle}>
+          <div className="board-row" style={rowStyle}>
+            <Square
+              position={0}
+              positions={this.state.positions}
+              onClick={this.handleClickSquare}
+            />
+            <Square
+              position={1}
+              positions={this.state.positions}
+              onClick={this.handleClickSquare}
+            />
+            <Square
+              position={2}
+              positions={this.state.positions}
+              onClick={this.handleClickSquare}
+            />
+          </div>
+          <div className="board-row" style={rowStyle}>
+            <Square
+              position={3}
+              positions={this.state.positions}
+              onClick={this.handleClickSquare}
+            />
+            <Square
+              position={4}
+              positions={this.state.positions}
+              onClick={this.handleClickSquare}
+            />
+            <Square
+              position={5}
+              positions={this.state.positions}
+              onClick={this.handleClickSquare}
+            />
+          </div>
+          <div className="board-row" style={rowStyle}>
+            <Square
+              position={6}
+              positions={this.state.positions}
+              onClick={this.handleClickSquare}
+            />
+            <Square
+              position={7}
+              positions={this.state.positions}
+              onClick={this.handleClickSquare}
+            />
+            <Square
+              position={8}
+              positions={this.state.positions}
+              onClick={this.handleClickSquare}
+            />
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+class Game extends React.Component {
+  render() {
+    return (
+      <div className="game">
+        <div className="game-board">
+          <Board />
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Game;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './css/index.css';
-import App from './App';
+import Game from './App';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Game />
   </React.StrictMode>,
   document.getElementById('root')
 );
